83fa76ca02b4274b38f46886e0eccf2160fee60b
Duongwphan <duongp2607@gmail.com>
2021-07-08 16:37:13 +0700

    Reactjs- Use props in component

--- src/App.js
+++ src/App.js
@@ -2,49 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function Hello() {
-  return (
-    <div>
-      <h1> Hello world </h1>
-      <h2> Wellcome to React-Js</h2>
-      <h3> 4 Cách định nghĩa function </h3>
-    </div>
-  )
-}
-function One() {
-  return (
-    <div>
-      <h4> Cách 1 sử dụng function kiểu thông thường </h4>
-    </div>
-  )
-}
-var Two = function () {
-  return (
-    <div>
-      <h4> Cách 2 sử dụng function gán biến </h4>
-    </div>
-  )
-}
-var Three = () => (
-  <div>
-    <h4> Cách 3 sử dụng function kiểu ES6 </h4>
-  </div>
-);
-//rcjc
-class Four extends React.Component {
-  render() {
-    return <button><h4>Cách 4 sử dụng class {this.props.name}</h4></button>;
-  }
-}
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Hello />
         <p>
-          Để bắt đầu học vui lòng truy cập  <code>src/App.js</code> chỉnh sửa và lưu lại
+          Hello, Wellcome to React js
         </p>
         <a
           className="App-link"
@@ -55,12 +19,40 @@ function App() {
           Learn React
         </a>
       </header>
-      <One />
-      <Two></Two>
-      <Three></Three>
-      <hr />
-      <Four />
-      
+
+      <div className="container">
+        <div className="row">
+
+          <div className="col-4">
+            <div className="card">
+              <img className="card-img-top" src="https://place-hold.it/300" alt=""></img>
+              <div className="card-body">
+                <h4 className=" cart-title">Title</h4>
+                <p className="cart-text">Text</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <img className="card-img-top" src="https://place-hold.it/300" alt=""></img>
+              <div className="card-body">
+                <h4 className=" cart-title">Title</h4>
+                <p className="cart-text">Text</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="card">
+              <img className="card-img-top" src="https://place-hold.it/300" alt=""></img>
+              <div className="card-body">
+                <h4 className=" cart-title">Title</h4>
+                <p className="cart-text">Text</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
